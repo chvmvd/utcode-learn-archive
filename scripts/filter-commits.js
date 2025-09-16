8bb0eafe3165b041ed0674541876159aa16ba620
WATAHIKI YUTO <chvmvd@gmail.com>
2025-09-16 12:33:14 +0900

    refactor

---
+++ scripts/filter-commits.js
@@ -0,0 +1,14 @@
+import { readFileSync } from "node:fs";
+
+const commits = JSON.parse(readFileSync("./gh-pages/commits.json", "utf-8"));
+
+const filteredCommits = commits
+  .filter((commit) => {
+    if (existsSync(`./gh-pages/commit/${commit.commitHash}`)) {
+      return false;
+    }
+    return true;
+  })
+  .slice(0, 50);
+
+writeFileSync("./filtered-commits.json", JSON.stringify(filteredCommits));
